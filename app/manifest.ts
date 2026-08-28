399162884f1f34177ba5d768e77206419a31bfed
nosmeri <parkdohyeon622@gmail.com>
2026-08-28 11:40:11 +0900

    feat: add PWA manifest icons and update configuration in manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,9 +11,22 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000000',
     icons: [
       {
-        src: '/favicon.ico',
-        sizes: '256x256',
-        type: 'image/x-icon',
+        src: '/icons/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: '/icons/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: '/icons/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
       },
     ],
   };
